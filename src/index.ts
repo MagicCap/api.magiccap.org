072919759123226e74ffd0e6ecd1d9bc1309288e
Jake Gealer <jake.gealer@krystal.uk>
2022-05-24 00:42:00 +0100

    add request logging

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { router } from "./routes";
+
+// Setup logging.
+import Morgan from "morgan";
+router.use(Morgan("tiny"));
+
+// Start the server.
 const port = process.env.PORT || 3000;
 router.listen(port, () => {
     console.log(`Listening on port ${port}`);
