2deb8d31b943adedd4faf0acb0d84730d36929ec
Jake Gealer <jake.gealer@krystal.uk>
2022-05-24 00:46:07 +0100

    move logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { router } from "./routes";
 
-// Setup logging.
-import Morgan from "morgan";
-router.use(Morgan("tiny"));
-
 // Start the server.
 const port = process.env.PORT || 3000;
 router.listen(port, () => {
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,3 +1,7 @@
 import express from "express";
 
 export const router = express();
+
+// Setup logging.
+import Morgan from "morgan";
+router.use(Morgan("tiny"));
